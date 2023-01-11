e5434d0174296382adcddf71cda0d5dd86824e6e
Savio <savio.96@hotmail.com>
2023-01-10 21:34:31 -0300

    feat(footer): Criação do component de desenvolvimento

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,7 @@
 import React from "react";
-import { Header } from "../sectors/Header/Header";
-import { Address } from "../components/Address/Address";
-import { Payments, VtexPci } from "../components/Payments/Payments";
-const Home = () => {
-  return (
-    <div>
-      {
-        //<Header />
-        //<Address />
-      }
-      <Payments></Payments>
 
-      <VtexPci></VtexPci>
-    </div>
-  );
+const Home = () => {
+  return <div>{}</div>;
 };
 
 export { Home };
