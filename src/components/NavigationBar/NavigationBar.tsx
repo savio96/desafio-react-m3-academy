93e2b90796a9f2f3e4d26593d11680adf99600d8
Savio <savio.96@hotmail.com>
2023-01-17 16:55:01 -0300

    feat(router): Adicionando Rotas

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -30,47 +30,19 @@ const NavigationBar = () => {
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
+              <a className="nav-link " aria-current="page" href="#">
+                CURSOS
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Features
+                SAIBA MAIS
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Pricing
+                INSTITUCIONAIS
               </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown link
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
             </li>
           </ul>
         </div>
--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,10 +1,20 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import React from "react";
-
-const Home = () => {
-  //const style = { background: "black" };
-  return <div /*style={style}*/></div>;
+import { Route, BrowserRouter } from "react-router-dom";
+import { Home } from "../../pages/Home";
+/*
+import Home from "./Home";
+import Sobre from "./Sobre";
+import Usuario from "./Usuario";
+<Route component={Home} path="/" exact />
+      <Route component={Sobre} path="/sobre" />
+      <Route component={Usuario} path="/usuario" />
+*/
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Route path="/" element={<Home />} />
+    </BrowserRouter>
+  );
 };
 
-export { Home };
+export { Routes };
