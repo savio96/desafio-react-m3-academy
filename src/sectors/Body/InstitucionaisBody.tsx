9d943d37007f628f1974eb9ed56e7f82b17a2e64
Savio <savio.96@hotmail.com>
2023-01-19 13:05:34 -0300

    feat(form): Adicionado o accordion no body

--- src/sectors/Body/InstitucionaisBody.tsx
+++ src/sectors/Body/InstitucionaisBody.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { AccordionBody } from "../../components/AccordionBody/AccordionBody";
 const InstitucionaisBody = () => {
   return (
-    <div>
-      <h2>INSTITUCIONAL</h2>
+    <div className="institucional-wrapper">
+      <h2 className="institucional-title">INSTITUCIONAL</h2>
+      <AccordionBody></AccordionBody>
     </div>
   );
 };
