ea6ea69a0b5516dc8d9480e63eddd0d767f39559
Savio <savio.96@hotmail.com>
2023-01-19 23:41:43 -0300

    feat(form): Adicionado os estilos mobile do formulário

---
+++ src/sectors/Body/SaibaBody.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SaibaBody = () => {
+  return (
+    <div className="body-wrapper">
+      <h2 className="body-title">SAIBA MAIS</h2>
+    </div>
+  );
+};
+
+export { SaibaBody };
